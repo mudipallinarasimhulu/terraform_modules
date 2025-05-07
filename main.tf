a90b10f677fd7cec799e9caee9a7dc114c2b1d6d
name <mudipallinarasimhulu@gmail.com>
2025-05-07 23:59:05 +0530

    second commit

--- main.tf
+++ main.tf
@@ -1,10 +1,27 @@
 #AWS vpc resource 
 resource "aws_vpc" "Web_VPC" {
   cidr_block = var.cidr_vpc
+
   enable_dns_support = true
+
   enable_dns_hostname = true 
 
   tags {
      Enviroment = var.enviroment_tags
+ }
+
 }
+
+# aws internet gateway 
+
+resource "aws_internet_gateway" "web_ig" {
+
+vpc.id = var.aws_vpc.web_vpc
+
+  tags {
+     Enviroment = var.enviroment_tags
+ }
+}
+
+# aws subnet for vpc
 
---
+++ variable.tf
@@ -0,0 +1,5 @@
+varibale "cidr_vpc" {
+  type = string 
+
+}
+
